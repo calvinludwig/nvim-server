e7401ab577b22ae315d7fbbeb00fa069f3e6635c
Calvin Ludwig <git@calvinludwig.xyz>
2024-12-04 12:07:33 -0300

    treesitter

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,6 +4,22 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
+      require("catppuccin").setup({
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        transparent_background = true,
+        integrations = {
+          cmp = false,
+          gitsigns = false,
+          nvimtree = false,
+          treesitter = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = "",
+          },
+        },
+      })
+
       vim.cmd.colorscheme("catppuccin")
     end,
   },
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,6 +4,9 @@ return {
     config = function()
       local statusline = require("mini.statusline")
       statusline.setup({ use_icons = true })
+
+      local basics = require("mini.basics")
+      basics.setup({ options = { extra_ui = true } })
     end,
   },
 }
